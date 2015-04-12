db783da59c48ad2809ad9bb203fce4bf7e84b9bc
Emily Sas <emily.bronwen.sas@gmail.com>
2015-04-12 19:34:31 +0100

    rewriting test

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -14,24 +14,24 @@ describe('GitUserSearchController', function() {
 		expect(scope.searchResult).toBeUndefined();
 		expect(scope.searchTerm).toBeUndefined();
 	});
-});
+	
+	describe ('when searching for a user', function() {
 
-describe ('when searching for a user', function() {
+		var items = [
+			{
+				"login": "tansaku",
+				"avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+				"html_url": "https://github.com/tansaku"
+			},
+			{
+				"login": "stephenlloyd",
+				"avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+		    "html_url": "https://github.com/stephenlloyd"
+			}
+		];
 
-	var items = [
-		{
-			"login": "tansaku",
-			"avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-			"html_url": "https://github.com/tansaku"
-		},
-		{
-			"login": "stephenlloyd",
-			"avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-	    "html_url": "https://github.com/stephenlloyd"
-		}
-	];
-
-	it('displays search results', function() {
-		expect(scope.searchResult.items).toEqual(items);
+		it('displays search results', function() {
+			expect(scope.searchResult.items).toEqual(items);
+		})
 	})
-})
+});
